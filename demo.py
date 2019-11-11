5842ec3ff947145fda55b46fc93d09bc3aba9094
blank-27 <55805028+blank-27@users.noreply.github.com>
2019-11-11 18:08:06 +0530

    Some basic Array things done

---
+++ demo.py
@@ -0,0 +1,10 @@
+import numpy as np
+numbers_=[[687,68,34],[89,77,82],[556,99,92]]
+# np.array(numbers_)
+numbers_=np.array(numbers_)
+n1=numbers_.ndim
+print("\n",n1)
+n2=numbers_.size
+print("\n",n2)
+n3=numbers_.dtype
+print("\n",n3)
